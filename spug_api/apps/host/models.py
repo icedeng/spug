4681080a9fb9883e2f376ebd4bc3b67003712744
ice.deng <ice.deng@gmail.com>
2023-07-01 18:31:32 +0800

    ssh超时更改无效。。。

--- spug_api/apps/host/models.py
+++ spug_api/apps/host/models.py
@@ -24,13 +24,9 @@ class Host(models.Model, ModelMixin):
     def private_key(self):
         return self.pkey or AppSetting.get('private_key')
 
-    @property
-    def ssh_connect_timeout(self):
-        return AppSetting.get_default("ssh_connect_timeout", 60)
-
     def get_ssh(self, pkey=None, default_env=None):
         pkey = pkey or self.private_key
-        return SSH(self.hostname, self.port, self.username, pkey, default_env=default_env, connect_timeout=self.ssh_connect_timeout)
+        return SSH(self.hostname, self.port, self.username, pkey, default_env=default_env)
 
     def to_view(self):
         tmp = self.to_dict()
